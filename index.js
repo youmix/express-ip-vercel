b2c28da8fdf6e1911f4caae1ce2478fb5d873909
youmix <hatena66@gmail.com>
2022-11-27 15:09:17 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const axios = require("axios").default;
+const express = require("express");
+const app = express();
+
+app.get("/", async (req, res) => {
+    try {
+      const response = await axios.get('https://api.ipify.org');
+      console.log(response);
+      const ip = response.data;
+      res.send(ip);
+    } catch (error) {
+      console.error(error);
+      res.send("データがうまく取得できませんでした。");
+    }
+  });
+
+//app.listen(process.env.PORT || 3000);
+app.listen({ port: 3000 }, () => {
+    console.log(`Server ready at http://localhost:3000`);
+  });
+  console.log('starts');
+
+//export default app;
